95307bc622bb2a8567fc64ba51aab5aa2ccd5bbd
Casey Schaufler <casey@schaufler-ca.com>
2018-11-28 15:22:51 -0800

    LSM: Use lsm_export in the secctx_to_secid hooks
    Convert the secctx_to_secid hooks to use the lsm_export
    structure instead of a u32 secid. There is some scaffolding
    involved that will be removed when security_secctx_to_secid()
    is updated.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/apparmor/include/secid.h
+++ security/apparmor/include/secid.h
@@ -27,7 +27,8 @@ struct aa_label;
 
 struct aa_label *aa_secid_to_label(struct lsm_export *l);
 int apparmor_secid_to_secctx(struct lsm_export *l, char **secdata, u32 *seclen);
-int apparmor_secctx_to_secid(const char *secdata, u32 seclen, u32 *secid);
+int apparmor_secctx_to_secid(const char *secdata, u32 seclen,
+			     struct lsm_export *l);
 void apparmor_release_secctx(char *secdata, u32 seclen);
 
 
