e7fc7716c7fd83c5cba70f79d75969bc445498fa
Casey Schaufler <casey@schaufler-ca.com>
2018-12-05 14:32:16 -0800

    NET: Remove netfilter scaffolding for lsm_export
    Remove scaffolding functions from the netfilter code.
    Replace with direct access to lsm_export fields so as
    to be explicit about how the secmarks are being
    handled.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- net/netfilter/nfnetlink_queue.c
+++ net/netfilter/nfnetlink_queue.c
@@ -317,8 +317,12 @@ static u32 nfqnl_get_sk_secctx(struct sk_buff *skb, char **secdata)
 	read_lock_bh(&skb->sk->sk_callback_lock);
 
 	if (skb->secmark) {
-		lsm_export_to_all(&le, skb->secmark);
-		security_secid_to_secctx(skb->secmark, secdata, &seclen);
+		/* Whichever LSM may be using the secmark */
+		lsm_export_init(&le);
+		le.flags = LSM_EXPORT_SELINUX | LSM_EXPORT_SMACK;
+		le.selinux = skb->secmark;
+		le.smack = skb->secmark;
+		security_secid_to_secctx(&le, secdata, &seclen);
 	}
 
 	read_unlock_bh(&skb->sk->sk_callback_lock);
