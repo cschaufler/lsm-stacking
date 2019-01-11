df7e19f0c449c795f75e53f5ae73600ae8e88ca4
Casey Schaufler <cschaufler@schaufler-ca.com>
2019-01-11 13:21:23 -0800

    LSM: Use lsm_context in security_inode_setsecctx
    Convert security_inode_setsecctx to use the lsm_context structure
    instead of a context/secid pair. There is some scaffolding involved
    that will be removed when the related data is updated.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -439,7 +439,7 @@ void security_release_secctx(struct lsm_context *cp);
 
 void security_inode_invalidate_secctx(struct inode *inode);
 int security_inode_notifysecctx(struct inode *inode, struct lsm_context *cp);
-int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32 ctxlen);
+int security_inode_setsecctx(struct dentry *dentry, struct lsm_context *cp);
 int security_inode_getsecctx(struct inode *inode, struct lsm_context *cp);
 #else /* CONFIG_SECURITY */
 
@@ -1237,7 +1237,8 @@ static inline int security_inode_notifysecctx(struct inode *inode,
 {
 	return -EOPNOTSUPP;
 }
-static inline int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32 ctxlen)
+static inline int security_inode_setsecctx(struct dentry *dentry,
+					   struct lsm_context *cp)
 {
 	return -EOPNOTSUPP;
 }
