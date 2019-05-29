c071d454c4006d4fd1e48fde3e56f30d444c8591
Casey Schaufler <casey@schaufler-ca.com>
2019-05-29 15:07:56 -0700

    LSM: Use lsm_export in the getpeersec_dgram hooks
    Convert the getpeersec_dgram hooks to use the lsm_export
    structure instead of a u32 secid. There is some scaffolding
    involved that will be removed when security_getpeersec_dgram()
    is updated.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -881,7 +881,7 @@
  *	ancillary message type.
  *	@sock contains the peer socket. May be NULL.
  *	@skb is the sk_buff for the packet being queried. May be NULL.
- *	@secid pointer to store the secid of the packet.
+ *	@l is a pointer to a buffer in which to copy the security data
  *	Return 0 on success, error on failure.
  * @sk_alloc_security:
  *	Allocate and attach a security structure to the sk->sk_security field,
@@ -1702,7 +1702,8 @@ union security_list_options {
 					char __user *optval,
 					int __user *optlen, unsigned len);
 	int (*socket_getpeersec_dgram)(struct socket *sock,
-					struct sk_buff *skb, u32 *secid);
+					struct sk_buff *skb,
+					struct lsm_export *l);
 	int (*sk_alloc_security)(struct sock *sk, int family, gfp_t priority);
 	void (*sk_free_security)(struct sock *sk);
 	void (*sk_clone_security)(const struct sock *sk, struct sock *newsk);
