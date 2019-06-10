990f8a3e88d78fa17b4c7e0a2bd2e50014d5f331
Casey Schaufler <casey@schaufler-ca.com>
2019-06-10 09:54:58 -0700

    LSM: Use lsmblob in security_secctx_to_secid
    Change security_secctx_to_secid() to fill in a lsmblob instead
    of a u32 secid. Multiple LSMs may be able to interpret the
    string, and this allows for setting whichever secid is
    appropriate. In some cases there is scaffolding where other
    interfaces have yet to be converted.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- net/netfilter/nft_meta.c
+++ net/netfilter/nft_meta.c
@@ -576,21 +576,20 @@ static const struct nla_policy nft_secmark_policy[NFTA_SECMARK_MAX + 1] = {
 
 static int nft_secmark_compute_secid(struct nft_secmark *priv)
 {
-	u32 tmp_secid = 0;
+	struct lsmblob le;
 	int err;
 
-	err = security_secctx_to_secid(priv->ctx, strlen(priv->ctx), &tmp_secid);
+	err = security_secctx_to_secid(priv->ctx, strlen(priv->ctx), &le);
 	if (err)
 		return err;
 
-	if (!tmp_secid)
-		return -ENOENT;
-
-	err = security_secmark_relabel_packet(tmp_secid);
+	/* Using le[1] is scaffolding */
+	err = security_secmark_relabel_packet(le.secid[1]);
 	if (err)
 		return err;
 
-	priv->secid = tmp_secid;
+	/* Using le[1] is scaffolding */
+	priv->secid = le.secid[1];
 	return 0;
 }
 
