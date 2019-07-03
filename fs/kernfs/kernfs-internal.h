a3f5726872d61fc5ff1b1676aa27ddee41be0c9e
Casey Schaufler <casey@schaufler-ca.com>
2019-07-03 08:41:11 -0700

    LSM: Use lsmcontext in security_inode_getsecctx
    Change the security_inode_getsecctx() interface to fill
    a lsmcontext structure instead of data and length pointers.
    This provides the information about which LSM created the
    context so that security_release_secctx() can use the
    correct hook. A lsmcontext is used within kernfs to store
    the security information as well.
    
    security_inode_getsecctx() does not use the "display"
    value. There's only one possible user at any time at
    this point, and the use is only internal.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- fs/kernfs/kernfs-internal.h
+++ fs/kernfs/kernfs-internal.h
@@ -21,8 +21,7 @@
 
 struct kernfs_iattrs {
 	struct iattr		ia_iattr;
-	void			*ia_secdata;
-	u32			ia_secdata_len;
+	struct lsmcontext	ia_context;
 
 	struct simple_xattrs	xattrs;
 };
--- include/linux/security.h
+++ include/linux/security.h
@@ -489,7 +489,7 @@ void security_release_secctx(struct lsmcontext *cp);
 void security_inode_invalidate_secctx(struct inode *inode);
 int security_inode_notifysecctx(struct inode *inode, void *ctx, u32 ctxlen);
 int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32 ctxlen);
-int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen);
+int security_inode_getsecctx(struct inode *inode, struct lsmcontext *cp);
 #else /* CONFIG_SECURITY */
 
 static inline int call_lsm_notifier(enum lsm_event event, void *data)
@@ -1290,7 +1290,8 @@ static inline int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32
 {
 	return -EOPNOTSUPP;
 }
-static inline int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen)
+static inline int security_inode_getsecctx(struct inode *inode,
+					   struct lsmcontext *cp)
 {
 	return -EOPNOTSUPP;
 }
