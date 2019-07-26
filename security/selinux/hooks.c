0b1bb5836e2ef1be25697f6f826e39ee38ab5a90
Casey Schaufler <casey@schaufler-ca.com>
2019-07-26 09:36:20 -0700

    NET: Add SO_PEERCONTEXT for multiple LSMs
    The getsockopt SO_PEERSEC provides the LSM based security
    information for a single module, but for reasons of backward
    compatibility cannot include the information for multiple
    modules. A new option SO_PEERCONTEXT is added to report the
    security "context" of multiple modules using a "compound" format
    
    	lsm1\0value\0lsm2\0value\0
    
    This is expected to be used by system services, including dbus-daemon.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4923,10 +4923,8 @@ static int selinux_socket_sock_rcv_skb(struct sock *sk, struct sk_buff *skb)
 	return err;
 }
 
-static int selinux_socket_getpeersec_stream(struct socket *sock,
-					    char __user *optval,
-					    int __user *optlen,
-					    unsigned int len)
+static int selinux_socket_getpeersec_stream(struct socket *sock, char **optval,
+					    int *optlen, unsigned int len)
 {
 	int err = 0;
 	char *scontext;
@@ -4946,18 +4944,12 @@ static int selinux_socket_getpeersec_stream(struct socket *sock,
 	if (err)
 		return err;
 
-	if (scontext_len > len) {
+	if (scontext_len > len)
 		err = -ERANGE;
-		goto out_len;
-	}
+	else
+		*optval = scontext;
 
-	if (copy_to_user(optval, scontext, scontext_len))
-		err = -EFAULT;
-
-out_len:
-	if (put_user(scontext_len, optlen))
-		err = -EFAULT;
-	kfree(scontext);
+	*optlen = scontext_len;
 	return err;
 }
 
