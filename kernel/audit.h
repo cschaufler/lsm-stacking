a1aa761058586145d4f66f20033e124aa8cbd1d5
Casey Schaufler <casey@schaufler-ca.com>
2021-07-14 12:16:28 -0700

    LSM: Use lsmblob in security_cred_getsecid
    Change the security_cred_getsecid() interface to fill in a
    lsmblob instead of a u32 secid. The associated data elements
    in the audit sub-system are changed from a secid to a lsmblob
    to accommodate multiple possible LSM audit users.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Cc: linux-integrity@vger.kernel.org
    Cc: linux-audit@redhat.com

--- kernel/audit.h
+++ kernel/audit.h
@@ -12,6 +12,7 @@
 #include <linux/fs.h>
 #include <linux/audit.h>
 #include <linux/skbuff.h>
+#include <linux/security.h>
 #include <uapi/linux/mqueue.h>
 #include <linux/tty.h>
 
@@ -137,7 +138,7 @@ struct audit_context {
 	kuid_t		    target_auid;
 	kuid_t		    target_uid;
 	unsigned int	    target_sessionid;
-	u32		    target_sid;
+	struct lsmblob	    target_lsm;
 	char		    target_comm[TASK_COMM_LEN];
 
 	struct audit_tree_refs *trees, *first_trees;
