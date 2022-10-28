468bf31355e5264d2171d4057113471f5070bd35
Casey Schaufler <casey@schaufler-ca.com>
2022-10-28 08:33:37 -0700

    lsm: wireup syscalls lsm_self_attr and lsm_module_list
    Wireup two syscalls for Linux Security Modules.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -886,8 +886,14 @@ __SYSCALL(__NR_futex_waitv, sys_futex_waitv)
 #define __NR_set_mempolicy_home_node 450
 __SYSCALL(__NR_set_mempolicy_home_node, sys_set_mempolicy_home_node)
 
+#define __NR_lsm_self_attr 451
+__SYSCALL(__NR_lsm_self_attr, sys_lsm_self_attr)
+
+#define __NR_lsm_module_list 452
+__SYSCALL(__NR_lsm_module_list, sys_lsm_module_list)
+
 #undef __NR_syscalls
-#define __NR_syscalls 451
+#define __NR_syscalls 453
 
 /*
  * 32 bit systems traditionally used different
