b485b5ba6c171951664147fbdd389cffa28e1679
Casey Schaufler <casey@schaufler-ca.com>
2022-11-10 10:53:35 -0800

    LSM: Identify modules by more than name
    Create a struct lsm_id to contain identifying information
    about Linux Security Modules (LSMs). At inception this contains
    the name of the module and an identifier associated with the
    security module. Change the security_add_hooks() interface to
    use this structure. Change the individual modules to maintain
    their own struct lsm_id and pass it to security_add_hooks().
    
    The values are for LSM identifiers are defined in a new UAPI
    header file linux/lsm.h. Each existing LSM has been updated to
    include it's LSMID in the lsm_id.
    
    The LSM ID values are sequential, with the oldest module
    LSM_ID_CAPABILITY being the lowest value and the existing modules
    numbered in the order they were included in the main line kernel.
    This is an arbitrary convention for assigning the values, but
    none better presents itself. The value 0 is defined as being invalid.
    The values 1-99 are reserved for any special case uses which may
    arise in the future.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1602,6 +1602,18 @@ struct security_hook_heads {
 	#undef LSM_HOOK
 } __randomize_layout;
 
+/**
+ * struct lsm_id - identify a Linux Security Module.
+ * @lsm: Name of the LSM. Must be approved by the LSM maintainers.
+ * @id: LSM ID number from uapi/linux/lsm.h
+ *
+ * Contains the information that identifies the LSM.
+ */
+struct lsm_id {
+	const u8	*lsm;
+	u32		id;
+};
+
 /*
  * Security module hook list structure.
  * For use with generic list macros for common operations.
@@ -1610,7 +1622,7 @@ struct security_hook_list {
 	struct hlist_node		list;
 	struct hlist_head		*head;
 	union security_list_options	hook;
-	const char			*lsm;
+	struct lsm_id			*lsmid;
 } __randomize_layout;
 
 /*
@@ -1645,7 +1657,7 @@ extern struct security_hook_heads security_hook_heads;
 extern char *lsm_names;
 
 extern void security_add_hooks(struct security_hook_list *hooks, int count,
-				const char *lsm);
+			       struct lsm_id *lsmid);
 
 #define LSM_FLAG_LEGACY_MAJOR	BIT(0)
 #define LSM_FLAG_EXCLUSIVE	BIT(1)
---
+++ include/uapi/linux/lsm.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/*
+ * Linux Security Modules (LSM) - User space API
+ *
+ * Copyright (C) 2022 Casey Schaufler <casey@schaufler-ca.com>
+ * Copyright (C) 2022 Intel Corporation
+ */
+
+#ifndef _UAPI_LINUX_LSM_H
+#define _UAPI_LINUX_LSM_H
+
+/*
+ * ID values to identify security modules.
+ * A system may use more than one security module.
+ *
+ * Values 1-99 are reserved for future use in special cases.
+ */
+#define LSM_ID_INVALID		0
+#define LSM_ID_CAPABILITY	100
+#define LSM_ID_SELINUX		101
+#define LSM_ID_SMACK		102
+#define LSM_ID_TOMOYO		103
+#define LSM_ID_IMA		104
+#define LSM_ID_APPARMOR		105
+#define LSM_ID_YAMA		106
+#define LSM_ID_LOADPIN		107
+#define LSM_ID_SAFESETID	108
+#define LSM_ID_LOCKDOWN		109
+#define LSM_ID_BPF		110
+#define LSM_ID_LANDLOCK		111
+
+#endif /* _UAPI_LINUX_LSM_H */
