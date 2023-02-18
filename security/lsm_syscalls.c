f53288a3309b88bb1df72a7751fd7ff7fb9d2f49
Casey Schaufler <casey@schaufler-ca.com>
2023-02-17 17:31:04 -0800

    LSM: syscalls for current process attributes
    Create a system call lsm_get_self_attr() to provide the security
    module maintained attributes of the current process.
    Create a system call lsm_set_self_attr() to set a security
    module maintained attribute of the current process.
    Historically these attributes have been exposed to user space via
    entries in procfs under /proc/self/attr.
    
    The attribute value is provided in a lsm_ctx structure. The structure
    identifys the size of the attribute, and the attribute value. The format
    of the attribute value is defined by the security module. A flags field
    is included for LSM specific information. It is currently unused and must
    be 0. The total size of the data, including the lsm_ctx structure and any
    padding, is maintained as well.
    
    struct lsm_ctx {
            __u64   id;
            __u64   flags;
            __u64   len;
            __u64   ctx_len;
            __u8    ctx[];
    };
    
    Two new LSM hooks are used to interface with the LSMs.
    security_getselfattr() collects the lsm_ctx values from the
    LSMs that support the hook, accounting for space requirements.
    security_setselfattr() identifies which LSM the attribute is
    intended for and passes it along.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

---
+++ security/lsm_syscalls.c
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * System calls implementing the Linux Security Module API.
+ *
+ *  Copyright (C) 2022 Casey Schaufler <casey@schaufler-ca.com>
+ *  Copyright (C) 2022 Intel Corporation
+ */
+
+#include <asm/current.h>
+#include <linux/compiler_types.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/security.h>
+#include <linux/stddef.h>
+#include <linux/syscalls.h>
+#include <linux/types.h>
+#include <linux/lsm_hooks.h>
+#include <uapi/linux/lsm.h>
+
+struct attrs_map {
+	char *name;
+	int attrs;
+};
+
+static const struct attrs_map lsm_attr_names[] = {
+	{
+		.name = "current",
+		.attrs = LSM_ATTR_CURRENT,
+	},
+	{
+		.name = "exec",
+		.attrs = LSM_ATTR_EXEC,
+	},
+	{
+		.name = "fscreate",
+		.attrs = LSM_ATTR_FSCREATE,
+	},
+	{
+		.name = "keycreate",
+		.attrs = LSM_ATTR_KEYCREATE,
+	},
+	{
+		.name = "prev",
+		.attrs = LSM_ATTR_PREV,
+	},
+	{
+		.name = "sockcreate",
+		.attrs = LSM_ATTR_SOCKCREATE,
+	},
+};
+
+/**
+ * lsm_name_to_attr - map an LSM attribute name to its ID
+ * @name: name of the attribute
+ *
+ * Look the given @name up in the table of know attribute names.
+ *
+ * Returns the LSM attribute value associated with @name, or 0 if
+ * there is no mapping.
+ */
+u64 lsm_name_to_attr(const char *name)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(lsm_attr_names); i++)
+		if (!strcmp(name, lsm_attr_names[i].name))
+			return lsm_attr_names[i].attrs;
+	return 0;
+}
+
+/**
+ * sys_lsm_set_self_attr - Set current task's security module attribute
+ * @ctx: the LSM contexts
+ * @size: size of @ctx
+ * @flags: which attribute to set
+ *
+ * Sets the calling task's LSM context. On success this function
+ * returns 0. If the attribute specified cannot be set a negative
+ * value indicating the reason for the error is returned.
+ */
+SYSCALL_DEFINE3(lsm_set_self_attr, struct lsm_ctx __user *, ctx, size_t __user,
+		size, u64, flags)
+{
+	return security_setselfattr(flags, ctx, size);
+}
+
+/**
+ * sys_lsm_get_self_attr - Return current task's security module attributes
+ * @ctx: the LSM contexts
+ * @size: size of @ctx, updated on return
+ * @flags: which attribute to return
+ *
+ * Returns the calling task's LSM contexts. On success this
+ * function returns the number of @ctx array elements. This value
+ * may be zero if there are no LSM contexts assigned. If @size is
+ * insufficient to contain the return data -E2BIG is returned and
+ * @size is set to the minimum required size. In all other cases
+ * a negative value indicating the error is returned.
+ */
+SYSCALL_DEFINE3(lsm_get_self_attr, struct lsm_ctx __user *, ctx,
+		size_t __user *, size, u64, flags)
+{
+	return security_getselfattr(flags, ctx, size);
+}
