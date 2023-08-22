ae4c8ddd3194c8d5359847a336b4fd33e48a1a4c
Casey Schaufler <casey@schaufler-ca.com>
2023-08-22 15:00:29 -0700

    LSM: Identify modules by more than name
    Create a struct lsm_id to contain identifying information
    about Linux Security Modules (LSMs). At inception this contains
    the name of the module, an identifier associated with the security
    module and an integer member "attrs" which identifies the API
    related data associated with each security module. The initial set
    of features maps to information that has traditionaly been available
    in /proc/self/attr. They are documented in a new userspace-api file.
    Change the security_add_hooks() interface to use this structure.
    Change the individual modules to maintain their own struct lsm_id
    and pass it to security_add_hooks().
    
    The values are for LSM identifiers are defined in a new UAPI
    header file linux/lsm.h. Each existing LSM has been updated to
    include it's LSMID in the lsm_id.
    
    The LSM ID values are sequential, with the oldest module
    LSM_ID_CAPABILITY being the lowest value and the existing modules
    numbered in the order they were included in the main line kernel.
    This is an arbitrary convention for assigning the values, but
    none better presents itself. The value 0 is defined as being invalid.
    The values 1-99 are reserved for any special case uses which may
    arise in the future. This may include attributes of the LSM
    infrastructure itself, possibly related to namespacing or network
    attribute management. A special range is identified for such attributes
    to help reduce confusion for developers unfamiliar with LSMs.
    
    LSM attribute values are defined for the attributes presented by
    modules that are available today. As with the LSM IDs, The value 0
    is defined as being invalid. The values 1-99 are reserved for any
    special case uses which may arise in the future.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Cc: linux-security-module <linux-security-module@vger.kernel.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Serge Hallyn <serge@hallyn.com>
    Reviewed-by: Mickael Salaun <mic@digikod.net>
    Reviewed-by: John Johansen <john.johansen@canonical.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -43,6 +43,7 @@
 #include <linux/fs_parser.h>
 #include <linux/watch_queue.h>
 #include <linux/io_uring.h>
+#include <uapi/linux/lsm.h>
 #include "smack.h"
 
 #define TRANS_TRUE	"TRUE"
@@ -4871,6 +4872,11 @@ struct lsm_blob_sizes smack_blob_sizes __ro_after_init = {
 	.lbs_superblock = sizeof(struct superblock_smack),
 };
 
+const struct lsm_id smack_lsmid = {
+	.name = "smack",
+	.id = LSM_ID_SMACK,
+};
+
 static struct security_hook_list smack_hooks[] __ro_after_init = {
 	LSM_HOOK_INIT(ptrace_access_check, smack_ptrace_access_check),
 	LSM_HOOK_INIT(ptrace_traceme, smack_ptrace_traceme),
@@ -5077,7 +5083,7 @@ static __init int smack_init(void)
 	/*
 	 * Register with LSM
 	 */
-	security_add_hooks(smack_hooks, ARRAY_SIZE(smack_hooks), "smack");
+	security_add_hooks(smack_hooks, ARRAY_SIZE(smack_hooks), &smack_lsmid);
 	smack_enabled = 1;
 
 	pr_info("Smack:  Initializing.\n");
