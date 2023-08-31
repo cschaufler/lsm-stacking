4df31464de1055d344d3774e7a5c906b5c09049d
Casey Schaufler <casey@schaufler-ca.com>
2023-08-31 09:27:48 -0700

    LSM: Single calls in socket_getpeersec hooks
    security_socket_getpeersec_stream() and security_socket_getpeersec_dgram()
    can only provide a single security context or secid to their callers.
    Open code these two hooks to return the first hook provided. Because
    only one "major" LSM is allowed there will only be one hook in the list,
    with the excepton being BPF. BPF is not expected to be using these
    interfaces.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/security.c
+++ security/security.c
@@ -4694,8 +4694,16 @@ EXPORT_SYMBOL(security_sock_rcv_skb);
 int security_socket_getpeersec_stream(struct socket *sock, sockptr_t optval,
 				      sockptr_t optlen, unsigned int len)
 {
-	return call_int_hook(socket_getpeersec_stream, -ENOPROTOOPT, sock,
-			     optval, optlen, len);
+	struct security_hook_list *hp;
+	int ilsm = lsm_task_ilsm(current);
+
+	hlist_for_each_entry(hp, &security_hook_heads.socket_getpeersec_stream,
+			     list)
+		if (ilsm == LSM_ID_UNDEF || ilsm == hp->lsmid->id)
+			return hp->hook.socket_getpeersec_stream(sock, optval,
+								 optlen, len);
+
+	return -ENOPROTOOPT;
 }
 
 /**
@@ -4715,8 +4723,13 @@ int security_socket_getpeersec_stream(struct socket *sock, sockptr_t optval,
 int security_socket_getpeersec_dgram(struct socket *sock,
 				     struct sk_buff *skb, u32 *secid)
 {
-	return call_int_hook(socket_getpeersec_dgram, -ENOPROTOOPT, sock,
-			     skb, secid);
+	struct security_hook_list *hp;
+
+	hlist_for_each_entry(hp, &security_hook_heads.socket_getpeersec_dgram,
+			     list)
+		return hp->hook.socket_getpeersec_dgram(sock, skb, secid);
+
+	return -ENOPROTOOPT;
 }
 EXPORT_SYMBOL(security_socket_getpeersec_dgram);
 
